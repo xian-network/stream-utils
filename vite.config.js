67622dfc9647793235ec6d46511310e3b9e8f61a
duelingbenjos <getrektwithme1@gmail.com>
2025-06-19 12:03:35 +0100

    updaet

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: process.env.NODE_ENV === 'production' ? '/stream-utils/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
